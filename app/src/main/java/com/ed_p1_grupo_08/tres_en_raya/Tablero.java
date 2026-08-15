3d822c61d1452ca9ebed2f7405c7b185c165b8a0
dnatt8 <donatopradochulde2005@gmail.com>
2026-08-15 11:58:25 -0500

    Tablero

--- app/src/main/java/com/ed_p1_grupo_08/tres_en_raya/Tablero.java
+++ app/src/main/java/com/ed_p1_grupo_08/tres_en_raya/Tablero.java
@@ -3,7 +3,19 @@ package com.ed_p1_grupo_08.tres_en_raya;
 public class Tablero {
 
     int[] celdas;
+    int ocupadas;
 
+    public Tablero(){
+        celdas= new int[9];
+        ocupadas= 0;
+    }
+    public Tablero(int[] c, int o){ // para clonar tablero
+        celdas= c;
+        ocupadas= o;
+    }
+    public boolean isFull(){return ocupadas==9;}
+
+    public Tablero clonarTablero(){return new Tablero(this.celdas, this.ocupadas);}
 
 
 }

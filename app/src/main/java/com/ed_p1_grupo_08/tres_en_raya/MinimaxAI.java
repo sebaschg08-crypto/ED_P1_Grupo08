62fa05acecdad64a6238ea395e929ba4deeb968e
VAVBcode <vavillacisb@gmail.com>
2026-08-15 12:12:34 -0500

    avance minimax

--- app/src/main/java/com/ed_p1_grupo_08/tres_en_raya/MinimaxAI.java
+++ app/src/main/java/com/ed_p1_grupo_08/tres_en_raya/MinimaxAI.java
@@ -1,4 +1,32 @@
 package com.ed_p1_grupo_08.tres_en_raya;
-
+import java.util.List;
 public class MinimaxAI {
+    private CalculadoraUtilidad calculadora;
+    private Jugador jugadorPC;
+    private Jugador oponente;
+
+    public MinimaxAI(Jugador jugadorPC, Jugador oponente) {
+        this.calculadora = new CalculadoraUtilidad();
+        this.jugadorPC = jugadorPC;
+        this.oponente = oponente;
+    }
+
+    public Tablero obtenerMejorJugada(Tablero tableroActual) {
+        Tree<Tablero> arbol = new Tree<>(tableroActual);
+
+        List<Tablero> posiblesJugadasPC = tableroActual.generarSucesores(jugadorPC);
+
+        Tablero mejorJugada = null;
+        int maxUtilidadMinima = Integer.MIN_VALUE;
+
+        for (Tablero jugadaPC : posiblesJugadasPC) {
+            TreeNode<Tablero> nodoPC = new TreeNode<>(jugadaPC);
+            arbol.getRaiz().addHijo(nodoPC);
+
+            List<Tablero> posiblesJugadasOponente = jugadaPC.generarSucesores(oponente);
+
+            int minUtilidadFamilia = Integer.MAX_VALUE;
+
+
+    }
 }

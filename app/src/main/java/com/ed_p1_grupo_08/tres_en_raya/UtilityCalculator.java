1c5aa3ba804ff2c2d09975f4afc21a9d46a34a16
sebaschg08-crypto <sebaschg08@gmail.com>
2026-08-15 12:09:23 -0500

    Fix method signature in UtilityCalculator

--- app/src/main/java/com/ed_p1_grupo_08/tres_en_raya/UtilityCalculator.java
+++ app/src/main/java/com/ed_p1_grupo_08/tres_en_raya/UtilityCalculator.java
@@ -1,6 +1,5 @@
 package com.ed_p1_grupo_08.tres_en_raya;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class UtiliyCalculator {
-  public static int
+  public static int calculateP(Tablero tablero, char Jugador) {

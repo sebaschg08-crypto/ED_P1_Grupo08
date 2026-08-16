d79e57f36520849da5e36078e3e0de5c7d9001ad
VAVBcode <vavillacisb@gmail.com>
2026-08-16 10:52:49 -0500

    parte 1

--- app/src/main/java/com/ed_p1_grupo_08/tres_en_raya/ControladorJuego.java
+++ app/src/main/java/com/ed_p1_grupo_08/tres_en_raya/ControladorJuego.java
@@ -1,5 +1,9 @@
 package com.ed_p1_grupo_08.tres_en_raya;
 
 public class ControladorJuego {
-
+    private Tablero tablero;
+    private Jugador jugador1;
+    private Jugador jugador2;
+    private Jugador jugadorActual;
+    private MinimaxAI ia;
 }

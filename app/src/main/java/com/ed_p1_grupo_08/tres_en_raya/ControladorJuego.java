0970f94635c52226183be9dc3a7ab96d6e406f06
VAVBcode <vavillacisb@gmail.com>
2026-08-16 10:56:21 -0500

    parte 1

--- app/src/main/java/com/ed_p1_grupo_08/tres_en_raya/ControladorJuego.java
+++ app/src/main/java/com/ed_p1_grupo_08/tres_en_raya/ControladorJuego.java
@@ -6,4 +6,16 @@ public class ControladorJuego {
     private Jugador jugador2;
     private Jugador jugadorActual;
     private MinimaxAI ia;
+    public ControladorJuego(Jugador j1, Jugador j2, Jugador empieza) {
+        this.jugador1 = j1;
+        this.jugador2 = j2;
+        this.jugadorActual = empieza;
+        this.tablero = new Tablero();
+
+        if (j1.isEsComputadora() || j2.isEsComputadora()) {
+            Jugador pc = j1.isEsComputadora() ? j1 : j2;
+            Jugador humano = j1.isEsComputadora() ? j2 : j1;
+            this.ia = new MinimaxAI(pc, humano);
+        }
+    }
 }
